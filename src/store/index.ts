196c1cb551e1155ae7f72acd7b54c5b09a55eea6
dewasatriawan <dewa@bsa.id>
2022-06-28 23:53:24 +0800

    finish

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,18 @@
-import { Commit, createStore } from 'vuex'
-import { User } from '@/models/user'
+import {Commit, createStore} from 'vuex'
+import {User} from "@/models/user";
 
 export default createStore({
-  state: {
-    user: new User(),
-  },
-  mutations: {
-    SET_USER(state: { user: User }, user: User) {
-      state.user = user;
+    state: {
+        user: new User()
+    },
+    mutations: {
+        SET_USER(state: { user: User }, user: User) {
+            state.user = user;
+        }
+    },
+    actions: {
+        setUser({commit}: { commit: Commit }, user: User) {
+            return commit('SET_USER', user);
+        }
     }
-  },
-  actions: {
-    setUser({ commit }: { commit: Commit }, user: User) {
-      return commit('SET_USER', user);
-    }
-  }
 })
